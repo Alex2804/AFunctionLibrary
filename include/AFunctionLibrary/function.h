87d2ff9e7d9010252cedc5fc45f2280d0b4a0a6c
Alexander Muth <almu.hp@arcor.de>
2019-10-22 19:16:37 +0200

    Initial Commit

---
+++ include/AFunctionLibrary/function.h
@@ -0,0 +1,13 @@
+#ifndef AFUNCTIONLIBRARY_FUNCTION_H
+#define AFUNCTIONLIBRARY_FUNCTION_H
+
+namespace afl
+{
+    class Function
+    {
+
+    };
+}
+
+
+#endif //AFUNCTIONLIBRARY_FUNCTION_H
---
+++ src/function.cpp
@@ -0,0 +1,1 @@
+#include "AFunctionLibrary/function.h"
