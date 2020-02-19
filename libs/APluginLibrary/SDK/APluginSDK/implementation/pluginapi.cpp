bb474a9d62a4568fa3c4468677dad576676d9e37
Alexander Muth <almu.hp@arcor.de>
2020-02-19 20:52:48 +0100

    git subrepo pull libs/APluginLibrary
    subrepo:
      subdir:   "libs/APluginLibrary"
      merged:   "e07264f2"
    upstream:
      origin:   "https://github.com/Alex2804/APluginLibrary"
      branch:   "master"
      commit:   "e07264f2"
    git-subrepo:
      version:  "0.4.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a04d8c2"

--- libs/APluginLibrary/SDK/APluginSDK/implementation/pluginapi.cpp
+++ libs/APluginLibrary/SDK/APluginSDK/implementation/pluginapi.cpp
@@ -5,17 +5,33 @@
 // when pluginapi.cpp is compiled, all other source files should be compiled too
 #include "infomanager.cpp"
 
+#ifdef A_PLUGIN_SDK_DEBUG
+namespace apl
+{
+    namespace debug
+    {
+        extern "C"
+        {
+            size_t allocationCount = 0;
+            size_t freeCount = 0;
+        }
+    }
+}
+#endif //A_PLUGIN_SDK_DEBUG
+
 void* apl::allocateMemory(size_t size)
 {
 #ifdef A_PLUGIN_SDK_DEBUG
-    debug::allocationCount += 1;
+    if(size > 0)
+        debug::allocationCount += 1;
 #endif //A_PLUGIN_SDK_DEBUG
     return detail::allocateMemoryNotExported(size);
 }
 void apl::freeMemory(void *ptr)
 {
 #ifdef A_PLUGIN_SDK_DEBUG
-    debug::freeCount += 1;
+    if(ptr != nullptr)
+        debug::freeCount += 1;
 #endif //A_PLUGIN_SDK_DEBUG
     detail::freeMemoryNotExported(ptr);
 }
