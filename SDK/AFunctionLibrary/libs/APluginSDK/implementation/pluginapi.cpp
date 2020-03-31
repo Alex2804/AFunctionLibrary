8292658da358e35faaa28f6ea2005f82ed6bf2dd
Alexander Muth <almu.hp@arcor.de>
2020-04-01 01:46:54 +0200

    git subrepo clone https://github.com/Alex2804/APluginSDK SDK/AFunctionLibrary/libs/APluginSDK
    subrepo:
      subdir:   "SDK/AFunctionLibrary/libs/APluginSDK"
      merged:   "89aed364"
    upstream:
      origin:   "https://github.com/Alex2804/APluginSDK"
      branch:   "master"
      commit:   "89aed364"
    git-subrepo:
      version:  "0.4.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a04d8c2"

---
+++ SDK/AFunctionLibrary/libs/APluginSDK/implementation/pluginapi.cpp
@@ -0,0 +1,68 @@
+#ifdef A_PLUGIN_SDK_EXCLUDE_IMPLEMENTATION
+# include "../pluginapi.h"
+#endif
+
+// when pluginapi.cpp is compiled, all other source files should be compiled too
+#include "infomanager.cpp"
+
+#ifdef A_PLUGIN_SDK_DEBUG
+namespace apl
+{
+    namespace debug
+    {
+        extern "C"
+        {
+            size_t allocationCount = 0;
+            size_t freeCount = 0;
+        }
+    }
+}
+#endif //A_PLUGIN_SDK_DEBUG
+
+void* apl::allocateMemory(size_t size)
+{
+#ifdef A_PLUGIN_SDK_DEBUG
+    if(size > 0)
+        debug::allocationCount += 1;
+#endif //A_PLUGIN_SDK_DEBUG
+    return detail::allocateMemoryNotExported(size);
+}
+void apl::freeMemory(void *ptr)
+{
+#ifdef A_PLUGIN_SDK_DEBUG
+    if(ptr != nullptr)
+        debug::freeCount += 1;
+#endif //A_PLUGIN_SDK_DEBUG
+    detail::freeMemoryNotExported(ptr);
+}
+
+const apl::PluginInfo* apl::detail::getPluginInfo()
+{
+    return InfoManager::instance()->getPluginInfo();
+}
+
+size_t apl::detail::getPluginFeatureCount()
+{
+    return getPluginFeatureCountNotExported();
+}
+const apl::PluginFeatureInfo* apl::detail::getPluginFeatureInfo(size_t index)
+{
+    return getPluginFeatureInfoNotExported(index);
+}
+const apl::PluginFeatureInfo * const* apl::detail::getPluginFeatureInfos()
+{
+    return getPluginFeatureInfosNotExported();
+}
+
+size_t apl::detail::getPluginClassCount()
+{
+    return getPluginClassCountNotExported();
+}
+const apl::PluginClassInfo* apl::detail::getPluginClassInfo(size_t index)
+{
+    return getPluginClassInfoNotExported(index);
+}
+const apl::PluginClassInfo* const* apl::detail::getPluginClassInfos()
+{
+    return getPluginClassInfosNotExported();
+}
